08e82b1df8439bc1b54ca0db99fe2464e9108f0c
picolloo <lucaspicolloo@gmail.com>
2021-05-31 21:10:10 -0300

    correlation matrix (high definition)

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## References
 
 [Per instance algorithm selection](https://www.kaggle.com/c/per-instance-algorithm-selection)
+[Unsupervised Learning and Data Clustering](https://towardsdatascience.com/unsupervised-learning-and-data-clustering-eeecb78b422a)
--- src/index.py
+++ src/index.py
@@ -1,7 +1,32 @@
+
 # %% 
 
 import pandas as pd
+from sklearn import pipeline
+import numpy as np
+import seaborn as sns
 
 # %%
 
-df = pd.read_csv('./data/training.csv')
+df = pd.read_csv('../data/training.csv', nrows=1000)
+df.head()
+# %%
+
+features = df.iloc[:, 1:75]
+target = df.iloc[:, 75]
+
+features
+target
+# %%
+
+features.join(target)
+
+# %%
+
+corr = features.join(target).corr() 
+
+ax = sns.heatmap(corr,
+            xticklabels=corr.columns.values,
+            yticklabels=corr.columns.values)
+
+ax.set_title("Attributes Correlation Matrix", fontsize = 16)
