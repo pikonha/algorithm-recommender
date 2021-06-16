2813013fa586bd42041bde69b8304d19a11e2423
picolloo <lucaspicolloo@gmail.com>
2021-06-15 23:03:49 -0300

    data preparation

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 [Per instance algorithm selection](https://www.kaggle.com/c/per-instance-algorithm-selection)
 [Passo a passo para construir PIPELINES!](https://www.youtube.com/watch?v=LRmmtT20290)
 [Desvendando o PYCARET!!](https://www.youtube.com/watch?v=1CGaxTGGGck)
+[Repeated k-Fold Cross-Validation for Model Evaluation in Python](https://machinelearningmastery.com/repeated-k-fold-cross-validation-with-python/)****
--- src/index.py
+++ src/index.py
@@ -1,6 +1,7 @@
 
 # %% 
 
+from matplotlib.pyplot import axis
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import numpy as np
@@ -8,35 +9,79 @@ import seaborn as sns
 
 # %%
 
-df = pd.read_csv('../data/problem_solutions.csv')
+df = pd.read_csv(
+  '../data/problem_solutions.csv',
+  dtype={
+    'reference': str,
+    'problem_type': str,
+    'n_rows': int,
+    'n_columns': int,
+    'target_type': str,
+    'feature_type': str,
+    'type_of_learning': str,
+    'algorithm': str,
+    'URL': str
+  }
+)
 df.head()
 
 # %%
 df.shape
 
 # %%
+from functools import reduce 
 
-df = df.drop(columns=["URL"], axis=1)
+r = []
+for row in df['feature_type'].str.split(',').tolist():
+  c = {f'feature_{col}': 1 for col in row}
+  r.append(c)
+
+feature_types = pd.DataFrame(r).fillna(0)
 
 # %%
+df = df.drop(columns=["reference","URL", "feature_type"], axis=1)
 
-features = df.iloc[:,1:6]
-y = df.iloc[:, 6]
+# %%
+df.info()
+
+# %%
+df['algorithm'].value_counts()
+
+
+# %%
+df.head()
+# %%
+features =  pd.concat([feature_types, df], axis=1)
+features = features.iloc[:,:8]
+y = df.iloc[:, -1]
+
 
 # %%
 
 X = pd.get_dummies(features)
 X.head()
 
-# %%
 
-from sklearn.cluster import KMeans
+# %% 
+
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import KFold, cross_val_score
 from numpy import mean
 from numpy import std
 
 # %%
+
 kfold = KFold(n_splits=3, random_state=2, shuffle=True)
+
+model = DecisionTreeClassifier()
+model.fit(X, y)
+
+scores = cross_val_score(model, X, y, scoring='accuracy', cv=kfold, n_jobs=-1)
+print('Accuracy: %.3f (%.3f)' % (mean(scores), std(scores)))
+
+# %%
+
+from sklearn.cluster import KMeans
 
 # %%
 model = KMeans(n_clusters=4, random_state=1)
@@ -45,7 +90,6 @@ print('Accuracy: %.3f (%.3f)' % (mean(scores), std(scores)))
 
 # %%
 
-# df_f = pd.concat([X, y], axis=1)
 df_f = pd.get_dummies(df)
 df_f
 
