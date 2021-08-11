8f20d803a1c5c6e97090e61b728e60733fee4b78
picolloo <lucaspicolloo@gmail.com>
2021-08-10 22:35:43 -0300

    99% accuracy?????

--- src/index.py
+++ src/index.py
@@ -1,159 +1,201 @@
 
 # %% 
-
 import pandas as pd
-from sklearn.model_selection import train_test_split
-import numpy as np
-import seaborn as sns
+from sklearn import tree
 
 # %%
-
-df = pd.read_csv('../data/training.csv', nrows=1000)
+df = pd.read_csv(
+  '../data/text.csv',
+  dtype={
+    'reference': str,
+    'problem_type': str,
+    'n_rows': int,
+    'n_columns': int,
+    'target_type': str,
+    'feature_type': str,
+    'type_of_learning': str,
+    'text_content': str,
+    'algorithm': str,
+    'URL': str
+  }
+)
 df.head()
 
-# %%
-
-features = df.iloc[:, 1:75]
-target = df.iloc[:, 75]
-
-features
-target
-
-# df = features.join(target)
-# df
+#%%
+df.dropna(subset=["text_content", "feature_types"], inplace=True)
+df.shape
 
 # %%
-def find_correlation(data, threshold=0.9, remove_negative=False):
-    """
-    Given a numeric pd.DataFrame, this will find highly correlated features,
-    and return a list of features to remove.
-    Parameters
-    -----------
-    data : pandas DataFrame
-        DataFrame
-    threshold : float
-        correlation threshold, will remove one of pairs of features with a
-        correlation greater than this value.
-    remove_negative: Boolean
-        If true then features which are highly negatively correlated will
-        also be returned for removal.
-    Returns
-    --------
-    select_flat : list
-        listof column names to be removed
-    """
-    corr_mat = data.corr()
-    if remove_negative:
-        corr_mat = np.abs(corr_mat)
-    corr_mat.loc[:, :] = np.tril(corr_mat, k=-1)
-    already_in = set()
-    result = []
-    for col in corr_mat:
-        perfect_corr = corr_mat[col][corr_mat[col] > threshold].index.tolist()
-        if perfect_corr and col not in already_in:
-            already_in.update(set(perfect_corr))
-            perfect_corr.append(col)
-            result.append(perfect_corr)
-    select_nested = [f[1:] for f in result]
-    select_flat = [i for j in select_nested for i in j]
-    return select_flat
+df['algorithm'] = df['algorithm'].str.lower()
+df[['algorithm', 'problem_type']].value_counts()
 
 # %%
-
-# columns_to_remove = find_correlation(df)
-# columns_to_remove = ["sub_grade", *columns_to_remove]
-# numeric_vars=[col for col in df.drop(columns_to_remove, axis=1).columns if col != 'int_rate']
-# len(columns_to_remove)
-
-# %%
-# df.drop(columns_to_remove, axis=1)
+r = []
+for row in df['feature_types'].str.split(',').tolist():
+  c = {f'feature_{col}': 1 for col in row}
+  r.append(c)
+feature_types = pd.DataFrame(r).fillna(0).astype(int)
+feature_types.head()
 
 # %%
-
-X_train, X_test, y_train, y_test = train_test_split(features, target, random_state=1)
-
-# %%
-
-import sklearn.metrics
-import autosklearn.regression
+df.drop(columns=["reference","url", "feature_types"], axis=1, inplace=True)
 
 # %%
+continuous_features = df.iloc[:,1:3]
+continuous_features
+# %%
+discrete_features = pd.merge(
+  df.iloc[:,:1].reset_index(drop=True),
+  df.iloc[:,3:-2].reset_index(drop=True),
+  left_index=True, right_index=True
+) 
+discrete_features = pd.get_dummies(discrete_features)
+discrete_features.head()
+# %%
+X = pd.merge(
+  continuous_features.reset_index(drop=True), 
+  discrete_features.reset_index(drop=True),
+  left_index=True, right_index=True
+) 
+X.head()
 
-automl = autosklearn.regression.AutoSklearnRegressor(
-    time_left_for_this_task=120,
-    per_run_time_limit=30,
-    tmp_folder='/tmp/autosklearn_regression_example_tmp',
-    output_folder='/tmp/autosklearn_regression_example_out',
+# %%
+from sklearn.preprocessing import LabelEncoder
+
+target = df.iloc[:, -2]
+y = LabelEncoder().fit_transform(target)
+y
+
+#%%
+# ===========================================================================#
+
+## TEXT PROCESSING
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from collections import Counter
+
+nltk.download('stopwords')
+
+#%%
+stop_words = set(stopwords.words('english'))
+porter = PorterStemmer()
+def handle_text(text):
+  tokens = word_tokenize(text)
+  tokens = [
+    porter.stem(w) for w in tokens 
+    if not w in stop_words and len(w) > 2 and w.isalpha()]
+  return ' '.join(tokens)
+
+#%%
+def get_most_frequent_tokens(tokens, limit=10):
+  with_stp = Counter()
+  with_stp.update(tokens)
+  return [x for x,_ in with_stp.most_common(limit)]
+  
+#%%
+# tokenize
+tokens = df['text_content'].dropna().apply(handle_text)
+#%%
+tokens = tokens.apply(lambda x: x.split())
+#%%
+most_frequent_tokens = tokens.apply(lambda x: get_most_frequent_tokens(x, 15))
+most_frequent_tokens
+#%%
+df_tokens = pd.DataFrame.from_records(most_frequent_tokens)
+df_tokens.head(20)
+# %%
+w = []
+for _, row in df_tokens.iterrows():
+  w.append({col: 1 for col in row})
+
+df_tokens = pd.DataFrame(w).fillna(0).astype(int)
+df_tokens.head()
+
+# %%
+X = pd.merge(
+  X.reset_index(),
+  df_tokens.reset_index(),
+  left_index=True, right_index=True
 )
 
-automl.fit(features, target)
+# %% 
+# ===========================================================================#
+# SELECTING MOST RELEVANT FEATURES
+# from sklearn.feature_selection import SelectKBest, chi2
+
+# f = SelectKBest(score_func=chi2, k=5)
+# z = f.fit_transform(X, y)
+# cols = z.get_support(indices=True)
+# pd.DataFrame(X,y).head()
+
+# %%  
+# ===========================================================================#
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression, Perceptron
+from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+
+# %% 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
+# %% 
+from sklearn.naive_bayes import GaussianNB
+
+model = GaussianNB()  
+model.fit(X_train,y_train)
+score = model.score(X_test,y_test)
+print('The accuracy of the GaussianNB is', score)
 
 # %%
-print(automl.show_models())
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+score = model.score(X_test,y_test)
+print('The accuracy of the Decision Tree is', score)
+plot_tree(model)
 
+# %% 
+model = RandomForestClassifier(n_estimators=10, random_state=9, class_weight='balanced')
+model.fit(X_train, y_train)
+score = model.score(X_test,y_test)
+print('The accuracy of the Random Forest Model is', score)
 
-# %%
+#%%
+ppn = Perceptron(eta0=1, random_state=1)
+ppn.fit(X_train, y_train)
+y_pred = ppn.predict(X_test)
+accuracy_score(y_pred,y_test)
+score_ppn=cross_val_score(ppn, X,y, cv=5)
+score_ppn.mean()
+#%%
+# Gaussian Naive Bayes
+gaussian = GaussianNB()
+gaussian.fit(X_train, y_train)
+score_gaussian = gaussian.score(X_test,y_test)
+print('The accuracy of Gaussian Naive Bayes is', score_gaussian)
 
-predictions = automl.predict(X_test)
-print("R2 score:", sklearn.metrics.r2_score(y_test, predictions))
+#%%
+# Support Vector Classifier (SVM/SVC)
+from sklearn.svm import SVC
+svc = SVC()
+svc.fit(X_train, y_train)
+score_svc = svc.score(X_test,y_test)
+print('The accuracy of SVC is', score_svc)
 
+#%% Logistic Regression
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+score_logreg = logreg.score(X_test,y_test)
+print('The accuracy of the Logistic Regression is', score_logreg)
 
-
-# %%
-# corr = df_full.corr()
-# ax = sns.heatmap(corr,
-#             xticklabels=corr.columns.values,
-#             yticklabels=corr.columns.values)
-
-# ax.set_title("Attributes Correlation Matrix", fontsize = 16)
-
-# %%
-# corr_target = abs(corr["int_rate"])
-# corr_target
-
-# #Selecting highly correlated features
-# relevant_features = corr_target[corr_target>0.5]
-# relevant_features
-
-# %%
-# print(df[["grade","sub_grade"]].corr())
-
-
-
-
-
-# %%
-
-# df_test = pd.read_csv('../data/test.csv')
-# df_test.head()
-
-# # %% 
-# features_test = df_test.iloc[:, 1:75]
-# target_test = df_test.iloc[:, -1:]
-
-# target_test
-
-
-
-# from pycaret import classification as clf
-
-# # %%
-
-# ml_setup = clf.setup(
-#   data=df_full, 
-#   target="int_rate", 
-#   train_size=0.8,
-#   session_id=1234,
-#   numeric_features=numeric_vars
-# )
-
-# # %%
-# clf.compare_models(fold=5)
-
-# # %%
-# regression_results = clf.pull()
-# regression_results
-
-# # %%
-# model_metadata = clf.models()
-# model_metadata['Name'] # with ids
+#%% K-Nearest Neighbors
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+score_knn = knn.score(X_test,y_test)
+print('The accuracy of the KNN Model is',score_knn)
